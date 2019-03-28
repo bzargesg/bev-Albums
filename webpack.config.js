f7a34f80631b27048bddaf63cbb3050fd72e8e4f
bzargesg <bzargesg@gmail.com>
2019-03-27 18:35:29 -0600

    Configure webpack/package json for nodemon and webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 const webpack = require('webpack');
 const path = require('path');
-
+var SRC_DIR = path.join(__dirname, '/client/components');
+var DIST_DIR = path.join(__dirname, '/public');
 module.exports = {
   context: __dirname + '/client',
   entry: './index.js',
+  output: {
+    path: DIST_DIR,
+    filename: 'app.js'
+  },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
+        include: SRC_DIR,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'env']
-        },
-      },
-    ],
-  },
-  output: {
-    path: __dirname + '/public',
-    filename: 'app.js',
+        }
+      }
+    ]
   }
 };
