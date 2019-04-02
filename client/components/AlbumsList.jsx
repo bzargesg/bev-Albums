40761e524314256c5b29c9954d74cba574c0ae45
bzargesg <bzargesg@gmail.com>
2019-04-02 08:10:45 -0600

    refactor albumList for testing coverage

--- client/components/AlbumsList.jsx
+++ client/components/AlbumsList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Album from './Album.jsx';
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 class AlbumsList extends React.Component {
   constructor() {
     super();
@@ -26,73 +26,63 @@ class AlbumsList extends React.Component {
       })
       .then(res => {
         this.setState({ albums: res });
+      });
+
+    //get singles and EPs /data/epswithartist/artistname
+    fetch(`http://localhost:3242/data/epswithartist/${this.artistName}`, options)
+      .then(data => {
+        // console.log(data);
+        return data.json();
       })
-      .then(
-        //get singles and EPs /data/epswithartist/artistname
-        fetch(`http://localhost:3242/data/epswithartist/${this.artistName}`, options)
-          .then(data => {
-            // console.log(data);
-            return data.json();
-          })
-          .then(res => {
-            this.setState({ eps: res });
-          }),
-      )
-      .then(
-        //get compilations /data/compilationswithartist/artistname
-        fetch(`http://localhost:3242/data/compilationswithartist/${this.artistName}`, options)
-          .then(data => {
-            // console.log(data);
-            return data.json();
-          })
-          .then(res => {
-            this.setState({ compilations: res });
-          }),
-      )
-      .then(
-        //appears on /data/albumswithartist/artistname
-        fetch(`http://localhost:3242/data/albumswithartist/${this.artistName}`, options)
-          .then(data => {
-            // console.log(data);
-            return data.json();
-          })
-          .then(res => {
-            this.setState({ appearsOn: res });
-          }),
-      );
+      .then(res => {
+        this.setState({ eps: res });
+      });
+
+    //get compilations /data/compilationswithartist/artistname
+    fetch(`http://localhost:3242/data/compilationswithartist/${this.artistName}`, options)
+      .then(data => {
+        // console.log(data);
+        return data.json();
+      })
+      .then(res => {
+        this.setState({ compilations: res });
+      });
+
+    //appears on /data/albumswithartist/artistname
+    fetch(`http://localhost:3242/data/albumswithartist/${this.artistName}`, options)
+      .then(data => {
+        // console.log(data);
+        return data.json();
+      })
+      .then(res => {
+        this.setState({ appearsOn: res });
+      });
   }
-  //albums
-  //Singles and EPs
-  //compilations
-  //Appears On
-  //<Album picURL={} name={}/>
-  
+  mapAlbums(type) {
+    let element = [];
+    type.map(album =>
+      element.push(<Album picURL={album.image} name={album.name} artistName={this.artistName} />),
+    );
+    return element;
+  }
   render() {
     return (
       <div className="allAlbums" data-test="allAlbums">
         <h3>Albums</h3>
         <div className="albums albumType" data-test="albumsComponent">
-          {this.state.albums.map(album => {
-            return <Album picURL={album.image} name={album.name} artistName={this.artistName} />;
-          })}
+          {this.mapAlbums(this.state.albums)}
         </div>
         <h3>Singles and EPs</h3>
         <div className="eps albumType" data-test="epsComponent">
-          {this.state.eps.map(album => {
-            return <Album picURL={album.image} name={album.name} artistName={this.artistName} />;
-          })}
+          {this.mapAlbums(this.state.eps)}
         </div>
         <h3>Compilations</h3>
         <div className="compilations albumType" data-test="compilationsComponent">
-          {this.state.compilations.map(album => {
-            return <Album picURL={album.image} name={album.name} artistName={this.artistName} />;
-          })}
+          {this.mapAlbums(this.state.compilations)}
         </div>
         <h3>Appears On</h3>
         <div className="appearsOn albumType" data-test="appearsOnComponent">
-          {this.state.appearsOn.map(album => {
-            return <Album picURL={album.image} name={album.name} artistName={this.artistName} />;
-          })}
+          {this.mapAlbums(this.state.appearsOn)}
         </div>
       </div>
     );
