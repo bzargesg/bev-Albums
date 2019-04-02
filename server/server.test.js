b5ed86fd4730e834e5f45e1664fce1c71ffc6c58
bzargesg <bzargesg@gmail.com>
2019-04-02 16:40:58 -0600

    refactor router into seperate file for testing added hardcoded tests to test routing

---
+++ server/server.test.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+// const server = require('./server.js');
+
+const setUp = routeType => `http://localhost:3242/data/${routeType}/The Squirming Fever`;
+describe('Test routes ', () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  it('route: albumsbyartist ', () => {
+    expect.assertions(1);
+    const url = setUp('albumsbyartist');
+    return fetch(url, options)
+      .then(data => data.json())
+      .then(values => expect(values.length).toBe(15));
+  });
+
+  it('route: epswithartist ', () => {
+    expect.assertions(1);
+    const url = setUp('epswithartist');
+    return fetch(url, options)
+      .then(data => data.json())
+      .then(values => expect(values.length).toBe(6));
+  });
+  it('route: compilationswithartist ', () => {
+    expect.assertions(1);
+    const url = setUp('compilationswithartist');
+    return fetch(url, options)
+      .then(data => data.json())
+      .then(values => expect(values.length).toBe(3));
+  });
+  it('route: albumswithartist ', () => {
+    expect.assertions(1);
+    const url = setUp('albumswithartist');
+    return fetch(url, options)
+      .then(data => data.json())
+      .then(values => expect(values.length).toBe(1));
+  });
+});
