6d5613674b20690d0cb128cc35f4aabcfcd417c7
bzargesg <bzargesg@gmail.com>
2019-04-09 07:49:54 -0600

    add icon  mouseover

--- client/components/SidebarOptions.jsx
+++ client/components/SidebarOptions.jsx
@@ -1,21 +1,78 @@
 import React from 'react';
 
-const SidebarOptions = () => {
-  return (
-    <div className="SidebarOptionsContainer">
-      <div className="SidebarOptions" data-test="Home">
-        <img className="Home iconImg" alt="Home" src="images/home.png" />
-        <span className="optionText imgText">Home</span>
+class SidebarOptions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      HomeImg: 'images/home.png',
+      SearchImg: 'images/search.png',
+      LibImg: 'images/library.png',
+    };
+  }
+  mouseOver(type) {
+    console.log('mouseover');
+    switch (type) {
+      case 'Home':
+        this.setState({ HomeImg: 'images/homeW.png' });
+        break;
+      case 'Search':
+        this.setState({ SearchImg: 'images/searchW.png' });
+        break;
+      case 'Library':
+        this.setState({ LibImg: 'images/libraryW.png' });
+        break;
+      default:
+        break;
+    }
+  }
+  mouseLeave(type) {
+    console.log('mouseleave');
+    switch (type) {
+      case 'Home':
+        this.setState({ HomeImg: 'images/home.png' });
+        break;
+      case 'Search':
+        this.setState({ SearchImg: 'images/search.png' });
+        break;
+      case 'Library':
+        this.setState({ LibImg: 'images/library.png' });
+        break;
+      default:
+        break;
+    }
+  }
+  render() {
+    return (
+      <div className="SidebarOptionsContainer">
+        <div
+          className="SidebarOptions"
+          data-test="Home"
+          onMouseOver={this.mouseOver.bind(this, 'Home')}
+          onMouseLeave={this.mouseLeave.bind(this, 'Home')}
+        >
+          <img className="Home iconImg" alt="Home" src={this.state.HomeImg} />
+          <span className="optionText imgText">Home</span>
+        </div>
+        <div
+          className="SidebarOptions"
+          data-test="Search"
+          onMouseOver={this.mouseOver.bind(this, 'Search')}
+          onMouseLeave={this.mouseLeave.bind(this, 'Search')}
+        >
+          <img className="Search iconImg" alt="Search" src={this.state.SearchImg} />
+          <span className="optionText imgText">Search</span>
+        </div>
+        <div
+          className="SidebarOptions"
+          data-test="Your Library"
+          onMouseOver={this.mouseOver.bind(this, 'Library')}
+          onMouseLeave={this.mouseLeave.bind(this, 'Library')}
+        >
+          <img className="Library iconImg" alt="Library" src={this.state.LibImg} />
+          <span className="optionText imgText">Your Library</span>
+        </div>
       </div>
-      <div className="SidebarOptions" data-test="Search">
-        <img className="Search iconImg" alt="Search" src="images/search.png" />
-        <span className="optionText imgText">Search</span>
-      </div>
-      <div className="SidebarOptions" data-test="Your Library">
-        <img className="Library iconImg" alt="Library" src="images/library.png" />
-        <span className="optionText imgText">Your Library</span>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default SidebarOptions;
