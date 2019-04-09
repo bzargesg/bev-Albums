f05b202d8f37e448e2f684437b20a37ac65f72ea
bzargesg <bzargesg@gmail.com>
2019-04-09 11:51:56 -0600

    combine sidebar and albumslist

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,5 +2,13 @@ import AlbumsList from './AlbumsList.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './Sidebar.jsx';
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <AlbumsList />
+    </div>
+  );
+};
 // ReactDOM.render(<AlbumsList />, document.getElementById('app'));
-ReactDOM.render(<Sidebar />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -23,26 +23,28 @@ class Sidebar extends React.Component {
   }
   render() {
     return (
-      <div className="Sidebar" data-test="Sidebar">
-        <h3 className="SidebarHeader" data-test="SidebarHeader">
-          <img className="Logo" alt="Spoopify Logo" src="images/spotifly.png" />{' '}
-          <span className="logoTitle imgText">Spot-a-fly</span>
-        </h3>
-        <SidebarOptions />
-        <div className="RecentlyPlayed">Recently Played</div>
-        <SidebarRecents recents={this.recents} />
-        <div
-          className="Install"
-          data-test="Install App"
-          onMouseOver={this.mouseOver.bind(this)}
-          onMouseLeave={this.mouseLeave.bind(this)}
-        >
-          <img className="InstallIcon" alt="Install Icon" src={this.state.InstallIcon} />
-          <span className="InstallText imgText">Install App</span>
-        </div>
-        <div className="User" data-test="User">
-          <img className="User iconImg" alt="User Icon" src="images/user.png" />
-          <span className="userText imgText">User</span>
+      <div className="SidebarContainer">
+        <div className="Sidebar" data-test="Sidebar">
+          <h3 className="SidebarHeader" data-test="SidebarHeader">
+            <img className="Logo" alt="Spoopify Logo" src="images/spotifly.png" />{' '}
+            <span className="logoTitle imgText">Spot-a-fly</span>
+          </h3>
+          <SidebarOptions />
+          <div className="RecentlyPlayed">Recently Played</div>
+          <SidebarRecents recents={this.recents} />
+          <div
+            className="Install"
+            data-test="Install App"
+            onMouseOver={this.mouseOver.bind(this)}
+            onMouseLeave={this.mouseLeave.bind(this)}
+          >
+            <img className="InstallIcon" alt="Install Icon" src={this.state.InstallIcon} />
+            <span className="InstallText imgText">Install App</span>
+          </div>
+          <div className="User" data-test="User">
+            <img className="User iconImg" alt="User Icon" src="images/user.png" />
+            <span className="userText imgText">User</span>
+          </div>
         </div>
       </div>
     );
